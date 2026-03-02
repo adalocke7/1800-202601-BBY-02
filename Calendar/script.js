e331a60905920e394de2eeaf2abf4926ec341908
Jonathan-Lin-162 <ylin297@my.bcit.ca>
2026-03-02 00:46:46 -0800

    add some comments

--- Calendar/script.js
+++ Calendar/script.js
@@ -1,9 +1,14 @@
+// calendarBtn is for icon
+// calendarContainer is used to display and hide the calendar
 const btn = document.getElementById("calendarBtn");
 const calendar = document.getElementById("calendarContainer");
 
+// when the icon is clicked, the function works
 btn.addEventListener("click", function () {
-  if (calendar.style.display === "none") {
+  // if it is hidden, display it
+  if (calendar.style.display == "none") {
     calendar.style.display = "block";
+  // if it is displayed, hide it
   } else {
     calendar.style.display = "none";
   }
