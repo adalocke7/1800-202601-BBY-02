ad15b16e8b5e7fe3a6d57f373d3d4c73a79e216b
yuzuruus <72951653+yuzuruus@users.noreply.github.com>
2026-03-10 10:42:31 -0700

    fixed API key leak

--- Fifa terminology/firebaseConfigue.js
+++ Fifa terminology/firebaseConfigue.js
@@ -1,17 +1,17 @@
 // Import Firebase modules
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { db } from '../src/firebaseConfig.js';
 
-// Your Firebase config from the screenshot
-const firebaseConfig = {
-  apiKey: "AIzaSyDTYgM4NO4rchkqRgqhoDSfsLPu68luTaw",
-  authDomain: "fifa-web-app-1fb5d.firebaseapp.com",
-  projectId: "fifa-web-app-1fb5d",
-  storageBucket: "fifa-web-app-1fb5d.firebasestorage.app",
-  messagingSenderId: "520690646403",
-  appId: "1:520690646403:web:ee6e073e0ff3034610d4de",
-  measurementId: "G-J2GL22VQB6"
-};
+import { 
+  collection, 
+  getDocs, 
+  doc, 
+  getDoc, 
+  setDoc, 
+  deleteDoc, 
+  serverTimestamp 
+} from "firebase/firestore";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
